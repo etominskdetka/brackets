ec8a28ebf2885a89209517ac0093361af293c7d2
etominskdetka <9798dima@gmail.com>
2021-08-29 02:57:49 +0300

    feat: complete test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = str.split("");
+  let openBrackets = [];
+  let closeBrackets = [];
+  let stack = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (bracketsConfig[i][0] == bracketsConfig[i][1]) {
+      stack.push(bracketsConfig[i][0]);
+    }
+    {
+      openBrackets.push(bracketsConfig[i][0]);
+      closeBrackets.push(bracketsConfig[i][1]);
+    }
+  }
+
+  let calc = true;
+  let calcArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (stack.includes(arr[i])) {
+      calcArr.push(arr[i]);
+      if (calcArr[calcArr.length - 1] == calcArr[calcArr.length - 2]) {
+        calcArr.pop();
+        calcArr.pop();
+      }
+    } else if (openBrackets.includes(arr[i])) {
+      calcArr.push(arr[i]);
+    } else if (closeBrackets.includes(arr[i])) {
+      let idx = closeBrackets.indexOf(arr[i]);
+      if (calcArr[calcArr.length - 1] == openBrackets[idx]) {
+        calcArr.pop();
+      } else {
+        calc = false;
+      }
+    }
+  }
+  if (calcArr.length > 0) {
+    calc = false;
+  }
+  return calc;
+};
